dfd7491367ef61cf315890c4d99588a8b3f6d3a1
Anaxagorax <eric.leinad92@gmail.com>
2014-03-08 10:41:01 -0400

    Added Flask App and a tool.py file for functions and a templates folder

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+
+app = Flask(__name__) #initializing app
+
+##### Landing Page #####
+@app.route("/")
+def Landing():
+    
+    return "Landing Page"
+
+
+##### Upload Log Page #####
+@app.route("/uplog")
+def UploadLog():
+
+	return "Upload Log Form"
+
+##### Upload Signature Page #####
+@app.route("/upsig")
+def UploadLog():
+
+	return "Upload Signature Form"
+
+##### Analyze Page #####
+@app.route("/analize")
+def Analyze():
+
+	return "Analyze Log and Display output"
+
+##### Store Signature Page #####
+@app.route("/")
+def Signature():
+
+	return "Do something with signature"
+
+##### Upload Log Page #####
+@app.route("/upload")
+def UploadLog():
+
+	return "Upload Form"
+
+
+if __name__ == "__main__":
+    
+    app.run()
