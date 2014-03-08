70edb331e6b7f65b63e06846f9d3b4426c57ab94
elfeto <felipe.torres.cs@gmail.com>
2014-03-08 14:04:42 -0400

    No se porque se queja

--- tools.py
+++ tools.py
@@ -4,29 +4,65 @@
 #				   	#
 #####################
 
-def logliner():
-	logger = []
-	import hashlib
-	logfile = open('access_log', 'r')
-	for lines in logfile:
-		hashing(lines, logger)
 
-def hashing(lines, logger):
+import re
+import os.path
+import string
+import hashlib
+
+def buildHash(array):
+	md5 = hashlib.md5()
+	md5.update(array)
+	csig = md5.hexdigest()
+	return csig
+	
+
+def fileParser(path, signatures, keywords):
+	import hashlib
+	logfile = open(path, 'r')
+	for lines in logfile:
+		split_line = hashing(lines)
+		keys, sigs, error = parseLine(split_line, signatures,keywords)
+		#print logger
+
+def splitLine(lines):
 	lines = lines.split(' ')
 
 	dic = {}
 	dic['ip'] = lines[0]
-	logger.append(dic['ip']) #ip
 	date = lines[3]
 	dic['date'] = date[1:-1]
-	logger.append(dic['date']) #fecha
 	dic['type'] = lines[5]
-	logger.append(dic['type']) #type
 	dic['string'] = lines[6]
-	logger.append(dic['string']) #string
 	dic['proto'] = lines[7]
-	logger.append(dic['proto']) #protocolo
 	dic['id'] = lines[8]
-	logger.append(dic['id']) #access-id
+	
+	return dic
 
-logliner()
+def parseLine(line, signatures, keywords):
+	
+	key_found = []	
+	sig_found = []
+
+	# Check length of string (avoid exhaustive attack)
+	if len(line["url"]) > 256:
+		return None, None, "URL too long, possible attack"
+
+	for key in keywords:
+		if re.search(key, line["url"]):
+			key_found.append(key)
+
+		
+	splitted_url = line["url"].split("/")
+	url_len = len(splitted_url)
+	print splitted_url
+	for i in range(url_len):
+		current_str = string.join(splitted_url[i:], "/")
+		c_sig = buildHash(current_str)
+		if c_sig in signatures:
+			sig_found.append((c_sig, current_str))	
+		print current_str, c_sig
+
+	return key_found, sig_found, None
+			
+	
