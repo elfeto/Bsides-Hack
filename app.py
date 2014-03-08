d79556bdbc260008ccdcedc9abcb257b78f7cb6b
acarrasquillo <abimael.carrasquillo@gmail.com>
2014-03-08 14:51:47 -0400

    Upload file form

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, render_template
 from tools import *
+import time
+from werkzeug.utils import secure_filename
+import os
 
+UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__) #initializing app
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ##### Landing Page #####
 @app.route("/")
@@ -13,7 +18,7 @@ def Landing():
 @app.route("/uplog")
 def UploadLog():
 
-	return "Upload Log Form"
+	return render_template('uploadlog.html')
 
 ##### Upload Signature Page #####
 @app.route("/upsig")
@@ -22,14 +27,33 @@ def UploadSig():
 	return "Upload Signature Form"
 
 ##### Analyze Page #####
-@app.route("/analyze")
+@app.route("/analyze", methods=['POST'])
 def Analyze():
+	#Get form file
+	file = request.files['log-file']
+	#Create secure filename
+	filename = "log-" + str(time.time())
+	#Save the file
+	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-	chart = GraphTime([('192.168.1.1', 23), ('192.168.2.1', 645)])
+	try:
 
-	#return chart
+		db= MySQLdb.connect(host="localhost",user="bsides", passwd="lamadredelquemerompaestepassword",db="Bsides")
 
-	return render_template("analyze.html", chart = str(chart))
+		c = db.cursor(MySQLdb.cursors.DictCursor)
+
+		quer = Queries()
+
+		var = fileParser(os.path.join(app.config['UPLOAD_FOLDER'], filename), quer.getSignatures(c), quer.getKeywords(c))
+
+		chart = GraphTime(var)
+
+	except:
+
+		pass
+
+	return render_template("analyze.html", chart = chart)
+	
 
 ##### Store Signature Page #####
 @app.route("/signature")
